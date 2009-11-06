983db2c2aeee6bb87c695eadd1084155e9da8a65
Graham Brooks <graham@grahambrooks.com>
2009-11-06 00:31:18 -0500

    Added unit test

---
+++ test/intercept/model/LogElementUnitTests.java
@@ -0,0 +1,14 @@
+package intercept.model;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import org.junit.Test;
+
+public class LogElementUnitTests {
+    @Test
+    public void logElementPreservesMessage() {
+        LogElement logElement = new LogElement("a message");
+
+        assertThat(logElement.getMessage(), is("a message"));
+    }
+}
