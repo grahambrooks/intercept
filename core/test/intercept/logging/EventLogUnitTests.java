069c3cc512d51cf448bdfef055b4d33bbd46e329
Graham Brooks <graham@grahambrooks.com>
2010-02-03 07:13:09 -0500

    Added a unit test around filtering logs.

--- core/test/intercept/logging/EventLogUnitTests.java
+++ core/test/intercept/logging/EventLogUnitTests.java
@@ -2,9 +2,16 @@ package intercept.logging;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+
+import intercept.model.By;
+import intercept.model.LogElement;
 import intercept.model.LogEntry;
 
+import intercept.model.LogFilter;
+import intercept.model.SimpleLogElement;
 import org.junit.Test;
+
+import java.util.List;
 
 public class EventLogUnitTests {
     @Test
@@ -22,5 +29,18 @@ public class EventLogUnitTests {
         assertThat(log.getEntries().size(), is(1));
         log.clear();
         assertThat(log.getEntries().size(), is(0));
+    }
+
+    @Test
+    public void canFilterLogs(){
+        EventLog log = new EventLog();
+        LogEntry logEntry = new LogEntry();
+        logEntry.add(new SimpleLogElement("Hi"));
+
+        log.add(logEntry);
+
+        List<LogElement> filteredElements = log.filtered(By.type(SimpleLogElement.class));
+
+        assertThat(filteredElements.size(), is(1));
     }
 }
