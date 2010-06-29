7d606a1a6b4940ae0cc46d88e238a866bd20faeb
Graham Brooks <graham@grahambrooks.com>
2010-06-29 12:25:56 -0400

    Simplistic autometon for end of header recognition

--- core/src/intercept/proxy/HTTPAutomaton.java
+++ core/src/intercept/proxy/HTTPAutomaton.java
@@ -1,16 +1,36 @@
 package intercept.proxy;
 
+import intercept.utils.Block;
+
+import java.util.HashMap;
+import java.util.Map;
+
 class HTTPAutomaton {
     private State currentState;
-    public HTTPAutomaton()
-    {
+    private Map<State, Block<byte[]>> delegates;
+
+    public HTTPAutomaton() {
         this.currentState = State.READING_HEADER;
+        delegates = new HashMap<State, Block<byte[]>>();
     }
+
     public State currentState() {
         return currentState;
     }
 
+    public void process(byte[] data) {
+        if (data[0] == '\n' && data[1] == '\n') {
+            delegates.get(State.HEADER_RECEIVED).yield(data);
+        } else {
+            delegates.get(currentState).yield(data);
+        }
+    }
+
+    public void setDelegate(State state, Block<byte[]> block) {
+        delegates.put(state, block);
+    }
+
     public enum State {
-        READING_HEADER
+        HEADER_RECEIVED, READING_HEADER
     }
 }
--- core/test/intercept/proxy/HTTPAutomatonTests.java
+++ core/test/intercept/proxy/HTTPAutomatonTests.java
@@ -1,5 +1,6 @@
 package intercept.proxy;
 
+import intercept.utils.Block;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -11,6 +12,44 @@ public class HTTPAutomatonTests {
         HTTPAutomaton automaton = new HTTPAutomaton();
 
         assertThat(automaton.currentState(), is(HTTPAutomaton.State.READING_HEADER));
+    }
+
+    @Test
+    public void automatonPassesHeaderDataToDelegate() {
+        HTTPAutomaton automaton = new HTTPAutomaton();
+
+        final byte[][] received = new byte[1][1];
+        automaton.setDelegate(HTTPAutomaton.State.READING_HEADER, new Block<byte[]>() {
+            @Override
+            public void yield(byte[] item) {
+                received[0] = item;
+            }
+        });
+
+        byte[] data = "hello".getBytes();
+
+        automaton.process(data);
+
+        assertThat(received[0], is(data));
+    }
+
+    @Test
+    public void automatonSignalsEndOfHeaderData() {
+        HTTPAutomaton automaton = new HTTPAutomaton();
+
+        final boolean[] received = new boolean[1];
+        automaton.setDelegate(HTTPAutomaton.State.HEADER_RECEIVED, new Block<byte[]>() {
+            @Override
+            public void yield(byte[] item) {
+                received[0] = true;
+            }
+        });
+
+        byte[] data = "\n\n".getBytes();
+
+        automaton.process(data);
+
+        assertThat(received[0], is(true));
     }
 
 }
